29002a0ee33abb0cfe44675bb26d53e1dad8c5ac
mohit01717 <mohitgagneja943@gmail.com>
2024-04-05 21:22:11 +0530

    readme updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,47 @@
 import React, { useContext } from "react";
 import Nav from "../components/Nav";
 import { MyContext } from "../MyContext";
-import Typewriter from 'react-typewriter-effect';
+import Typewriter from "typewriter-effect";
 
 const Home = () => {
-  
-  const {home} = useContext(MyContext);
+  const { home } = useContext(MyContext);
   return (
-    <div ref={home} className=" bg-gradient-to-r from-violet-500 to-fuchsia-500  h-[100vh] text-white  ">
+    <div
+      ref={home}
+      className=" bg-gradient-to-r from-violet-500 to-fuchsia-500  h-[100vh] text-white  "
+    >
       <Nav />
       <div className="h-[90vh] flex justify-center flex-col pl-6 lg:pl-20">
-   
-        <h1 className="text-[40px] xl:text-9xl md:text-7xl ">
-          Hello!
-        </h1>
-        <h1 className="text-[40px] xl:text-9xl md:text-7xl ">
-          I'm Mohit Gagneja
-        </h1>
-        <h4 className="text-[30px] xl:text-5xl md:text-4xl font-serif pt-4 ">
-          FullStack Developer
-        </h4>
+        <Typewriter
+          options={{
+            strings: "Hello",
+            autoStart: true,
+            loop: true,
+            wrapperClassName: "text-[40px] xl:text-9xl md:text-7xl ",
+            delay:0,
+            pauseFor:1500,
+          }}
+        />
+
+        <Typewriter
+          options={{
+            strings: "I'm Mohit Gagneja",
+            autoStart: true,
+            loop: true,
+           delay:50,
+            wrapperClassName: "text-[40px] xl:text-9xl md:text-7xl ",
+          }}
+        />
+
+        <Typewriter
+          options={{
+            strings: "FullStack Developer",
+            autoStart: true,
+            loop: true,
+            delay:100,
+            wrapperClassName: "text-[40px] xl:text-9xl md:text-7xl ",
+          }}
+        />
       </div>
     </div>
   );
